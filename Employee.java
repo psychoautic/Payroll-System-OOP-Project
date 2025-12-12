32f43c679c546dba5f31562fb27a98d1adb751ef
Anthony Edward <Anthony_ehab@hotmail.com>
2025-12-12 11:19:35 +0200

    added partTimeEmployee subclass

--- Employee.java
+++ Employee.java
@@ -110,3 +110,53 @@ class FullTimeEmployee extends Employee {
     }
 }
 
+class PartTimeEmployee extends Employee {
+
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(
+            String employeeId,
+            String firstName,
+            String lastName,
+            double baseSalary,
+            BankAccount bankAccount,
+            double hourlyRate,
+            int hoursWorked
+    ) {
+        super(employeeId, firstName, lastName, baseSalary, bankAccount);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+
+    public double calculatePay() {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+
+    public void displayEmployeeInfo() {
+        super.displayEmployeeInfo();
+        System.out.println("Hourly Rate: " + hourlyRate);
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Total Pay: " + calculatePay());
+    }
+}
